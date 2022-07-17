42d7f358213d02a3dec705361625a8afd46b12c0
Jeannine.Kwasnik <jeanninesworld@hotmail.com>
2022-07-17 09:18:56 -0400

    Cleaned up failing tests.

--- Framework/Pages/GoogleHomePage.cs
+++ Framework/Pages/GoogleHomePage.cs
@@ -8,15 +8,18 @@ using System.Threading.Tasks;
 
 namespace Framework.Pages
 {
-    public class GoogleHomePage :BasePage
+    public class GoogleHomePage : BasePage
     {
-        By _searchInput = By.XPath("//input[@title='Search']");
-        By _searchBtn = By.XPath("//input[@value='Google Search']");
-        public GoogleHomePage(IWebDriver driver) :base(driver){}
+        By _searchInput = By.XPath("//*[@aria-label='Search']");
+        By _searchBtn = By.XPath("//*[@class='FPdoLc lJ9FBc']//*[@value='Google Search']");
+        By _clickAway = By.XPath("//*[@jsmodel='sj77Re vWNDde P9Kqfe']");
+        public GoogleHomePage(IWebDriver driver) : base(driver) { }
 
         public GoogleResultsPage SearchText(String textToSearch)
         {
-            EnterText(_searchInput,TimeSpan.FromSeconds(5), "FishingGuru");
+           
+            EnterText(_searchInput, TimeSpan.FromSeconds(10), "FishingGuru");
+            Click(_clickAway, TimeSpan.FromSeconds(15));
             Click(_searchBtn, TimeSpan.FromSeconds(15));
             return new GoogleResultsPage(Driver);
         }
